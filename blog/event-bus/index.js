9ec85689d7c36e982656d7e62209a6364283ec71
(mastermind3001) <ayderumerov.com@gmail.com>
2023-12-19 13:10:31 +0300

    init services -without containerising

---
+++ blog/event-bus/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const events = [];
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+  axios.post('http://localhost:4003/events', event);
+
+  axios.post('http://localhost:4006/events', event);
+  axios.post('http://localhost:4007/events', event);
+
+  res.send({status: 'OK'});
+
+});
+
+app.get('/events', (req, res) => {
+
+  res.status(200).send(events);
+
+});
+
+const port = 4005;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
