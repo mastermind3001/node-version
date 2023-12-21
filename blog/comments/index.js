fdd06c94b9e68618a7d0009e75bd239189eca69e
(mastermind3001) <ayderumerov.com@gmail.com>
2023-12-21 21:29:29 +0300

    containerising

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -24,7 +24,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   const status = 'pending';
   comments.push({id: commentId, content, status});
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId, content,
@@ -50,7 +50,7 @@ app.post('/events', async (req, res) => {
       return comment.id === id;
     });
     comment.status = status;
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentUpdated',
       data: {
         id,
--- blog/query/index.js
+++ blog/query/index.js
@@ -56,7 +56,7 @@ const port = 4002;
 app.listen(port, async () => {
   console.log(`Listening on ${port}`);
 
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get('http://event-bus-srv:4005/events');
 
   for(let event of res.data) {
     console.log('Processing event: ', event.type);
